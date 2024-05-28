66ce3267d1afdb67ac1e1cf4e004888718bff3ea
lowndes96 <lowndes96@outlook.com>
2024-05-28 14:17:09 +0100

    added 404 error to get topics

--- app.js
+++ app.js
@@ -7,5 +7,14 @@ const {getAllTopics} =require('./controllers/topics.controller')
 
 app.get('/api/topics', getAllTopics)
 
+app.use((err,req,res,next) => {
+    if (err.msg){
+        res.status(err.status).send({'msg':err.msg})
+    }
+    else{
+        next(err)
+    }
+})
+
 
 module.exports = app
--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,8 +1,17 @@
 const {findALlTopics} = require('../models/topics.model')
 
 function getAllTopics(req,res,next){
-    findALlTopics().then((allTopics) => {
-        res.status(200).send({'topics':allTopics})
+    findALlTopics()
+    .then((allTopics) => {
+        if (allTopics.length> 0){
+            res.status(200).send({'topics':allTopics})
+        }
+        else{
+            return Promise.reject({status: 404, msg: 'No results Found'})
+        }
+    })
+    .catch((err) => {
+        next(err)
     })
 }
 
