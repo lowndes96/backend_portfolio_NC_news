102415e1193735d041538a692f2a40b56dd419a1
lowndes96 <lowndes96@outlook.com>
2024-05-29 15:10:47 +0100

    completed task 6 with 2 200 returns and one 400

--- models/topics.model.js
+++ models/topics.model.js
@@ -1,35 +1,67 @@
-const db = require('../db/connection')
-const fs = require('fs/promises')
+const db = require('../db/connection');
+const fs = require('fs/promises');
 
-function findALlTopics(){
-    return db
-    .query('SELECT * FROM topics')
-    .then((result => {
-        return result.rows
-    }))
+function findALlTopics() {
+  return db.query('SELECT * FROM topics').then((result) => {
+    return result.rows;
+  });
 }
 
-function findArticleById(articleId){
-    return db.query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
+function findArticleById(articleId) {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
     .then((result) => {
-        return result.rows
-    })
+      return result.rows;
+    });
 }
 
-function findAllArticles(){
-    // const commentCount = db.query(`SELECT COUNT(body) AS comment_count FROM comments WHERE `)
-    return db.query(`SELECT articles.*, COUNT(comments.comment_id) AS comment_count
+function findAllArticles() {
+  // const commentCount = db.query(`SELECT COUNT(body) AS comment_count FROM comments WHERE `)
+  return db
+    .query(
+      `SELECT articles.*, COUNT(comments.comment_id) AS comment_count
     FROM articles 
     LEFT JOIN comments ON articles.article_id = comments.article_id 
     GROUP BY articles.article_id
-    ORDER BY articles.created_at DESC`)
+    ORDER BY articles.created_at DESC`
+    )
     .then((result) => {
-        const editedOutput = result.rows.map((article) => {
-            delete article.body
-            return article
-        })
-        return editedOutput
-    })
+      const editedOutput = result.rows.map((article) => {
+        delete article.body;
+        return article;
+      });
+      return editedOutput;
+    });
 }
 
-module.exports = {findALlTopics, findArticleById,findAllArticles}
+function findCommentsByArticle(articleId) {
+  const id = [articleId];
+//   console.log('find comms')
+  return db
+    .query(
+      `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC`,
+      id
+    )
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {return Promise.reject({'status':400, 'msg':'No results Found*'})})
+}
+
+function checkArticleExists(articleId) {
+  return db
+  .query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
+  .then(({rows})=> {
+    if (!rows.length){
+        return Promise.reject({'status':400, 'msg':'No results Found'})
+    }
+  })
+}
+
+module.exports = {
+  findALlTopics,
+  findArticleById,
+  findAllArticles,
+  findCommentsByArticle,
+  checkArticleExists,
+};
