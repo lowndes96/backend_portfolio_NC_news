9807f591019037657c2a2a3f55ad6f95496f6fd0
lowndes96 <lowndes96@outlook.com>
2024-05-29 12:21:44 +0100

    happy path for GET /api/articles completed

--- models/topics.model.js
+++ models/topics.model.js
@@ -16,4 +16,16 @@ function findArticleById(articleId){
     })
 }
 
-module.exports = {findALlTopics, findArticleById}
+function findAllArticles(){
+    // const commentCount = db.query(`SELECT COUNT(body) AS comment_count FROM comments WHERE `)
+    return db.query(`SELECT articles.*, COUNT(comments.comment_id) AS comment_count
+    FROM articles 
+    LEFT JOIN comments ON articles.article_id = comments.article_id 
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC`)
+    .then((result) => {
+        return result.rows
+    })
+}
+
+module.exports = {findALlTopics, findArticleById,findAllArticles}
